98f695b9bbb630be40f38cfb998d2e9f3e0e4d66
VtXsuperkyle <160232706@student.rnngroup.ac.uk>
2024-09-23 09:45:55 +0100

    added dcrm folder

---
+++ dcrm/website/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from.forms import CreateUserForm, Loginform
+
+# Create your views here.
+
+
+from django.http import HttpResponse
+
+def home(request):
+    return render(request, "website/index.html")
+
+#register a user
+def register(request):
+    form = CreateUserForm()
+
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #returen redirect('')
+
+    context = {'form': form}
+
+    return render(request,'website/register.html', context=context)
